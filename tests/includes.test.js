f2120601ba64b8fdb062fec4831d38b5d07e7169
Pavel Viarbitski <verbipo@gmail.com>
2023-01-31 08:15:45 +0100

    refactor: remove dublicated line 'describe('drop', () => { line"

--- tests/includes.test.js
+++ tests/includes.test.js
@@ -1,52 +1,50 @@
 const includes = require('../methods/array/includes');
 
 describe('includes', () => {
-  describe('includes', () => {
-    it('should throw an error if fromIndex argument is not a number', () => {
-      expect(() => includes([1, 2, 3,], 3, 'not a number')).toThrowError(RangeError('fromIndex argument must be a number'));
-    });
-    it('should throw an error if first argument is not aa string, array, or object', () => {
-      expect(() => includes(3, 3,)).toThrowError(TypeError('collection must be a string, array, or object'));
-      expect(() => includes(NaN, NaN,)).toThrowError(TypeError('collection must be a string, array, or object'));
-      expect(() => includes(undefined, undefined)).toThrowError(TypeError('collection must be a string, array, or object'));
-      expect(() => includes(true, true)).toThrowError(TypeError('collection must be a string, array, or object'));
-    });
-    it('returns true if the value is found in the string', () => {
-      const str = 'abcdefg';
-      expect(includes(str, 'b')).toBe(true);
-      expect(includes(str, 'f', 2.2)).toBe(true);
-      expect(includes(str, 'cd', 1)).toBe(true);
-      expect(includes(str, 'efg', 2.2)).toBe(true);
-    });
-    it('returns false if the value is not found in the string', () => {
-      const str = 'abcdefg';
-      expect(includes(str, 'h')).toBe(false);
-      expect(includes(str, 'd', 4)).toBe(false);
-      expect(includes(str, 'fs', 1)).toBe(false);
-      expect(includes(str, 'bc', 4.7)).toBe(false);
-      expect(includes(str, 'eqwef', 3)).toBe(false);
-    });
-    it('returns true if the value is found in the array', () => {
-      const array = [1, 2, 3, 'f', undefined, NaN];
-      expect(includes(array, 2)).toBe(true);
-      expect(includes(array, 'f')).toBe(true);
-      expect(includes(array, undefined, 3.4)).toBe(true);
-      expect(includes(array, NaN, 2.2)).toBe(true);
-    });
-    it('returns false if the value is not found in the array', () => {
-      const array = [1, 2, 3, 'f', undefined, NaN];
-      expect(includes(array, 4)).toBe(false);
-      expect(includes(array, 1, 3)).toBe(false);
-      expect(includes(array, 'aa')).toBe(false);
-      expect(includes(array, null)).toBe(false);
-    });
-    it('returns true if the value is found in the object values', () => {
-      const obj = { a: 1, b: 2, c: 3 };
-      expect(includes(obj, 2)).toBe(true);
-    });
-    it('returns false if the value is not found in the object values', () => {
-      const obj = { a: 1, b: 2, c: 3 };
-      expect(includes(obj, 4)).toBe(false);
-    });
+  it('should throw an error if fromIndex argument is not a number', () => {
+    expect(() => includes([1, 2, 3,], 3, 'not a number')).toThrowError(RangeError('fromIndex argument must be a number'));
+  });
+  it('should throw an error if first argument is not aa string, array, or object', () => {
+    expect(() => includes(3, 3,)).toThrowError(TypeError('collection must be a string, array, or object'));
+    expect(() => includes(NaN, NaN,)).toThrowError(TypeError('collection must be a string, array, or object'));
+    expect(() => includes(undefined, undefined)).toThrowError(TypeError('collection must be a string, array, or object'));
+    expect(() => includes(true, true)).toThrowError(TypeError('collection must be a string, array, or object'));
+  });
+  it('returns true if the value is found in the string', () => {
+    const str = 'abcdefg';
+    expect(includes(str, 'b')).toBe(true);
+    expect(includes(str, 'f', 2.2)).toBe(true);
+    expect(includes(str, 'cd', 1)).toBe(true);
+    expect(includes(str, 'efg', 2.2)).toBe(true);
+  });
+  it('returns false if the value is not found in the string', () => {
+    const str = 'abcdefg';
+    expect(includes(str, 'h')).toBe(false);
+    expect(includes(str, 'd', 4)).toBe(false);
+    expect(includes(str, 'fs', 1)).toBe(false);
+    expect(includes(str, 'bc', 4.7)).toBe(false);
+    expect(includes(str, 'eqwef', 3)).toBe(false);
+  });
+  it('returns true if the value is found in the array', () => {
+    const array = [1, 2, 3, 'f', undefined, NaN];
+    expect(includes(array, 2)).toBe(true);
+    expect(includes(array, 'f')).toBe(true);
+    expect(includes(array, undefined, 3.4)).toBe(true);
+    expect(includes(array, NaN, 2.2)).toBe(true);
+  });
+  it('returns false if the value is not found in the array', () => {
+    const array = [1, 2, 3, 'f', undefined, NaN];
+    expect(includes(array, 4)).toBe(false);
+    expect(includes(array, 1, 3)).toBe(false);
+    expect(includes(array, 'aa')).toBe(false);
+    expect(includes(array, null)).toBe(false);
+  });
+  it('returns true if the value is found in the object values', () => {
+    const obj = { a: 1, b: 2, c: 3 };
+    expect(includes(obj, 2)).toBe(true);
+  });
+  it('returns false if the value is not found in the object values', () => {
+    const obj = { a: 1, b: 2, c: 3 };
+    expect(includes(obj, 4)).toBe(false);
   });
 });
--- tests/take.test.js
+++ tests/take.test.js
@@ -1,37 +1,35 @@
 const take = require('../methods/array/take');
 
 describe('take', () => {
-  describe('take', () => {
-    it('should throw an error if first argument is not an array', () => {
-      const notArray = 'not an array';
-      expect(() => take(notArray)).toThrowError(TypeError('First argument must be an array'));
-    });
-    it('should throw an error if second argument is not a integer', () => {
-      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      expect(() => take(array, 'not a number')).toThrowError(RangeError('Second argument must be a integer'));
-    });
-    it('returns the copy of array with only first element if n is not specified', () => {
-      expect(take([1, 2, 3])).toEqual([1]);
-    });
-    it('returns the first n elements of the array', () => {
-      expect(take([1, 2, 3], 2)).toEqual([1, 2]);
-      expect(take([1, 2, 3], 2.5)).toEqual([1, 2]);
-      expect(take([1, 2, 3, 4, 5], 3)).toEqual([1, 2, 3]);
-      expect(take([1, 2, 3, 4, 5], 3.8)).toEqual([1, 2, 3]);
-    });
-    it('returns an empty array if n is negative', () => {
-      expect(take([1, 2, 3], -1)).toEqual([]);
-      expect(take([1, 2, 3], -1.28)).toEqual([]);
+  it('should throw an error if first argument is not an array', () => {
+    const notArray = 'not an array';
+    expect(() => take(notArray)).toThrowError(TypeError('First argument must be an array'));
+  });
+  it('should throw an error if second argument is not a integer', () => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(() => take(array, 'not a number')).toThrowError(RangeError('Second argument must be a integer'));
+  });
+  it('returns the copy of array with only first element if n is not specified', () => {
+    expect(take([1, 2, 3])).toEqual([1]);
+  });
+  it('returns the first n elements of the array', () => {
+    expect(take([1, 2, 3], 2)).toEqual([1, 2]);
+    expect(take([1, 2, 3], 2.5)).toEqual([1, 2]);
+    expect(take([1, 2, 3, 4, 5], 3)).toEqual([1, 2, 3]);
+    expect(take([1, 2, 3, 4, 5], 3.8)).toEqual([1, 2, 3]);
+  });
+  it('returns an empty array if n is negative', () => {
+    expect(take([1, 2, 3], -1)).toEqual([]);
+    expect(take([1, 2, 3], -1.28)).toEqual([]);
 
-    });
-    it('returns an empty array if n is equal zero', () => {
-      expect(take([1, 2, 3], 0)).toEqual([]);
-      expect(take([1, 2, 3], 0.88)).toEqual([]);
+  });
+  it('returns an empty array if n is equal zero', () => {
+    expect(take([1, 2, 3], 0)).toEqual([]);
+    expect(take([1, 2, 3], 0.88)).toEqual([]);
 
-    });
-    it('returns the whole array if n is greater than the array length', () => {
-      expect(take([1, 2, 3], 10)).toEqual([1, 2, 3]);
-      expect(take([1, 2, 3], 10.8)).toEqual([1, 2, 3]);
-    });
+  });
+  it('returns the whole array if n is greater than the array length', () => {
+    expect(take([1, 2, 3], 10)).toEqual([1, 2, 3]);
+    expect(take([1, 2, 3], 10.8)).toEqual([1, 2, 3]);
   });
 });
